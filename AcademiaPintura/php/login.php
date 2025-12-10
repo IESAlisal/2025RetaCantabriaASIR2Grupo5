8008f52b6570cb45b8d82d97a93d49385df35b88
luke-sosa <lcardenass2401@educantabria.es>
2025-12-10 18:25:42 +0100

    Creacion del login + Logout de la pagina web

--- AcademiaPintura/php/login.php
+++ AcademiaPintura/php/login.php
@@ -1,2 +1,21 @@
 <?php
-// creacion de un archivo relacionada con las carpetas creadas
+// login.php
+session_start();
+require_once "funciones.php";
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+    header("Location: index.php");
+    exit;
+}
+
+$usuario = $_POST["usuario"] ?? "";
+$passwd  = $_POST["passwd"] ?? "";
+
+if (comprobarLogin($usuario, $passwd)) {
+    $_SESSION["usuario"] = $usuario;
+    header("Location: indice.php");
+    exit;
+} else {
+    header("Location: index.php?msg=" . urlencode("Usuario o contraseña incorrectos"));
+    exit;
+}
